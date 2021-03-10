ff599d594c158fa2eb0a763452deb9090ee27164
Caleb Martinez <cm185265@ncr.com>
2021-03-10 16:57:27 -0500

    PoseNet

--- camerademo/src/App.js
+++ camerademo/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 import CoverCameraComponent from "./cameraComponets/coverCameraComponent";
-
+import PoseNet from "./modelComponents/poseNetComponent";
 function App() {
+    const [demoSelected, selectDemo] = useState('Pose');
   return (
     <div className="App">
-     <CoverCameraComponent/>
+        {demoSelected ==='Pose'? <PoseNet/>:<CoverCameraComponent/>}
+
     </div>
   );
 }
