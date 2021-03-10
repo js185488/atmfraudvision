3f2541de2639961cf6ce0c1f7a954971e71ce3ba
Caleb Martinez <cm185265@ncr.com>
2021-03-10 17:38:38 -0500

    server, call npm startServer, two api calls /start /startPython

--- camerademo/src/App.js
+++ camerademo/src/App.js
@@ -1,12 +1,38 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 import CoverCameraComponent from "./cameraComponets/coverCameraComponent";
+import {callPythonCoverDemo} from './messages/callLocalServer'
 
 function App() {
-  return (
+    const [demo, setDemo] = useState(null);
+
+    return (
     <div className="App">
-     <CoverCameraComponent/>
-    </div>
+        {(demo==='webCoverDemo'?
+            <CoverCameraComponent/>: null)}
+        {(demo==='pyCoverDemo'?
+            <>Running PythonDemo, Wait for terminal to open</>: null)}
+        {(demo==='cppCoverDemo'?
+            <>Running cpp Demo, Wait for terminal to open</>: null)}
+        <div className="buttonContainer">
+            <button className="demoButtons" onClick={()=>{
+                setDemo('pyCoverDemo');
+                callPythonCoverDemo()
+            }}>
+                Cover camera (python)
+            </button>
+            <button className="demoButtons" onClick={()=>{
+                setDemo('cppCoverDemo')
+            }}>
+                Cover camera (cpp)
+            </button>
+            <button className="demoButtons"
+                    onClick={()=>{
+                setDemo('webCoverDemo')
+            }}>
+                Cover camera (web based)
+            </button>
+        </div>    </div>
   );
 }
 
