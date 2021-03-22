e5757ab75c0e11a486895a5bcff46794b3a6c2d7
Caleb Martinez <cm185265@ncr.com>
2021-03-22 11:17:46 -0400

    jackPottingDemo (in-development) by facial recognition

--- camerademo/src/App.js
+++ camerademo/src/App.js
@@ -1,38 +1,30 @@
 import React, {useState} from 'react'
 import './App.css';
 import CoverCameraComponent from "./cameraComponets/coverCameraComponent";
-import {callPythonCoverDemo} from './messages/callLocalServer'
-
+import FaceDetection from "./cameraComponets/api/LoiteringDetection";
+import ButtonSelectionComponent from "./Menu/MenuSelectionComponent";
 function App() {
     const [demo, setDemo] = useState(null);
 
     return (
+
     <div className="App">
+        <ButtonSelectionComponent setDemo={setDemo}/>
+        <div className="DemoContainer">
         {(demo==='webCoverDemo'?
             <CoverCameraComponent/>: null)}
+        {(demo==='loiteringDemo'?
+            <FaceDetection/>: null)}
+
         {(demo==='pyCoverDemo'?
             <>Running PythonDemo, Wait for terminal to open</>: null)}
         {(demo==='cppCoverDemo'?
             <>Running cpp Demo, Wait for terminal to open</>: null)}
-        <div className="buttonContainer">
-            <button className="demoButtons" onClick={()=>{
-                setDemo('pyCoverDemo');
-                callPythonCoverDemo()
-            }}>
-                Cover camera (python)
-            </button>
-            <button className="demoButtons" onClick={()=>{
-                setDemo('cppCoverDemo')
-            }}>
-                Cover camera (cpp)
-            </button>
-            <button className="demoButtons"
-                    onClick={()=>{
-                setDemo('webCoverDemo')
-            }}>
-                Cover camera (web based)
-            </button>
-        </div>    </div>
+           </div>
+    </div>
+
+
+
   );
 }
 
